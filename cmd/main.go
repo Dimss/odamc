ccfb5882b6ddf9f26b87e5f434344b6ca93ae7f2
Dmitry Kartsev <dkartsev@redhat.com>
2019-11-07 13:42:44 +0200

    add -c opetion to set config.json path

--- cmd/main.go
+++ cmd/main.go
@@ -6,26 +6,23 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "secroute",
-	Short: "SecRoute Webhook - enforce creation of secure routes",
+	Use:   "odamc",
+	Short: "Odamc - Ofek Dynamic Admission Controller",
 }
-
-
 
 func init() {
 	// Init config
 	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringP("kubeconfig", "k", "", "Path to kubeconfig file, default to $home/.kube/config")
-	if err := viper.BindPFlag("kubeconfig", rootCmd.PersistentFlags().Lookup("kubeconfig")); err != nil {
-		panic(err)
-	}
 	// Setup commands
 	rootCmd.AddCommand(runWebhookServerCmd)
+	rootCmd.PersistentFlags().StringP("configpath", "c", "", "Path to config directory with config.json file, default to . ")
+	if err := viper.BindPFlag("configpath", rootCmd.PersistentFlags().Lookup("configpath")); err != nil {
+		panic(err)
+	}
 	// Init log
 	logrus.SetOutput(os.Stdout)
 	logrus.SetReportCaller(true)
@@ -35,24 +32,16 @@ func initConfig() {
 	viper.SetConfigType("json")
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
-	viper.SetEnvPrefix("SECRO")
+	viper.SetEnvPrefix("ODAMC")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
-	kubeconfig := viper.GetString("kubeconfig")
-	if kubeconfig == "" {
-		// Check if kubeconfig file exists in user's HOME
-		kubeconfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
-		_, err := os.Stat(kubeconfig)
-		if os.IsNotExist(err) {
-			// The kubeconfig wasn't passed in and not found under user's home directory, assuming inClusterConfig mode
-			logrus.Info("Unable to find kubeconfig, assuming running inside K8S cluster, gonna use inClusterConfig")
-			viper.Set("kubeconfig", "useInClusterConfig")
-		} else {
-			// Use kubeconfig from user's home directory
-			logrus.Info("Gonna use kubeconfig from user's HOME directory")
-			viper.Set("kubeconfig", kubeconfig)
-		}
+	configPath := viper.GetString("configpath")
+	logrus.Infof("Configuration directory %v: ", configPath)
+	// If config flag is empty, assume config.json located in current directory
+	if configPath != "" {
+		viper.AddConfigPath(configPath)
 	}
+	viper.WatchConfig()
 	err := viper.ReadInConfig()
 	if err != nil {
 		logrus.Errorf("Unable to read config.json file, err: %s", err)
@@ -60,7 +49,9 @@ func initConfig() {
 	}
 }
 
+
 func main() {
+
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
